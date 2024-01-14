bf28c70406d5ad38f50487e7636947f0c80bad86
vinayakkannan <vk2364@columbia.edu>
2024-01-13 23:58:49 -0500

    songs

--- my-app/src/Components/NavBar.tsx
+++ my-app/src/Components/NavBar.tsx
@@ -12,7 +12,7 @@ const nav_bar_style: CSSProperties = {
 const NavBar = () => {
   return (
     <div style={nav_bar_style}>
-      <Chip label="About Me!" />
+      <Chip label="About Me!" variant="outlined" />
       <Chip label="Experiences" variant="outlined" />
       <Chip label="Projects" variant="outlined" />
     </div>
